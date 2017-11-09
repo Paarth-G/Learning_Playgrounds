2ebf2a688f7929a6f5a2ed98f295a617f0bbdc3a
Paarth-G <paarthgarg@hotmail.com>
2017-11-09 11:20:30 -0500

    Intro Playground

---
+++ Intro.playground/Contents.swift
@@ -0,0 +1,19 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+/*
+var str = "Hello, playground"
+
+str = "Hello"
+*/
+
+var name : Int = 5
+
+name = 6
+
+//constant (can't change)
+let MyOtherName = 7;
+
+var s: double_t = 55
+
+var a: String = "Hello World"
