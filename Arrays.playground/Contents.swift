bd1efe7fe65714fc48e8fdf43a49d7e2e7cf9f1d
Paarth-G <paarthgarg@hotmail.com>
2017-11-09 11:16:42 -0500

    Arrays Playground

---
+++ Arrays.playground/Contents.swift
@@ -0,0 +1,23 @@
+// ARRAYS
+
+var cars = ["Ferrari F50","Cadillac STS","Chrysler 300","Dodge Viper","Ford F350","Chevy Corvette","Chevy Camaro", "Dodge Dart","Cadillac Escalade","Dodge Durango"]
+
+cars[1]
+
+cars.count
+
+// Remove Last (Durango)
+cars.removeLast()
+
+// Remove #8 (Camaro)
+cars.remove(at: 8)
+
+cars
+
+// Add a car -- 2 ways
+cars.append("Caddy")
+cars.insert("Ford Mustand", at: 9)
+
+
+var scores = [67, 98, 76, 95, 53, 86, 110]
+
