da3a0bd428c178501594364443b61f5024bc07b3
Paarth-G <paarthgarg@hotmail.com>
2017-11-09 11:17:21 -0500

    Closure Playground

---
+++ closures.playground/Contents.swift
@@ -0,0 +1,15 @@
+// CLOSURES 
+
+var names = ["Tyler","Zach","Mike","Jack","Abby","Christina"]
+
+func alpha(name1: String, name2: String) -> Bool
+{
+    return name1 < name2
+}
+
+
+alpha("Zach",name2: "Christina")
+
+//var sortedNames = sorted(names,<)
+
+//sorted(names,>);
