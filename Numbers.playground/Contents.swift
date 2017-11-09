d0c891f0bc9cd0c05161de72b104e2ecc71db3af
Paarth-G <paarthgarg@hotmail.com>
2017-11-09 11:20:54 -0500

    Numbers Playground

---
+++ Numbers.playground/Contents.swift
@@ -0,0 +1,33 @@
+// Numbers
+
+var a = 7 //int
+var b = 21.9 //double
+var c : Float = 10.7 //float - need to specify
+
+var d = Int.max
+
+// Order of Ops for swift
+// *, /, +, -
+
+(4 - 3) * 10
+
+var number = 14
+
+if number <= 10 || number == 14
+{
+    print("Number = \(number).")
+}
+else
+{
+    print("\(number) does not meet the condition.")
+}
+
+var y = 12.5
+
+var z = b + Double(c) // can't add double and float or any other type
+
+a = 3
+var m = 16
+
+m/a //if you divide you don't get the remainder of ints 
+m%a //to get remainder
